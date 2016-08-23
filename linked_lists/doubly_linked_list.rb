a53f71aa11d941bb45165b381a068a718ba079a1
John Figueiredo <john.figueiredo@goodsearch.com>
2016-08-22 18:26:09 -0700

    Implement a doubly linked list

---
+++ linked_lists/doubly_linked_list.rb
@@ -0,0 +1,32 @@
+require 'pry'
+
+class Node
+  attr_accessor :node, :previous, :next
+
+  def initialize(node)
+    @node = node
+  end
+end
+
+node_1 = Node.new('node 1')
+node_2 = Node.new('node 2')
+node_3 = Node.new('node 3')
+node_4 = Node.new('node 4')
+
+node_1.next = node_2
+node_2.next = node_3
+node_3.next = node_4
+
+node_4.previous = node_3
+node_3.previous = node_2
+node_2.previous = node_1
+
+puts "Node 1 is: #{node_1.node}"
+puts "Next after node 1 is: #{node_1.next.node}"
+puts "Next after node 2 is: #{node_2.next.node}"
+puts "Next after node 3 is: #{node_3.next.node}"
+puts "Next after node 4 is: #{node_4.next.inspect}"
+puts "Next before node 4 is: #{node_4.previous.node}"
+puts "Next before node 3 is: #{node_3.previous.node}"
+puts "Next before node 2 is: #{node_2.previous.node}"
+puts "Next before node 1 is: #{node_1.previous.inspect}"
