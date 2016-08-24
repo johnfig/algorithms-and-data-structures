a0b6e501598bf94bfa05f52f88f6c96646fae063
John Figueiredo <pair+john.figueiredo@goodsearch.com>
2016-08-23 17:25:20 -0700

    Add SinglyLinkedList class with #insert_node

--- linked_lists/singly_linked_list.rb
+++ linked_lists/singly_linked_list.rb
@@ -1,10 +1,28 @@
 require 'pry'
 
-class Node
-  attr_accessor :node, :next
+# SinglyLinkedList has #insert_node method that appends it to the beginning.
+# This avoids a dom traversal that results from breaking existing linked lists
+# and rebuilding them.
 
-  def initialize(node)
-    @node = node
+class Node
+  attr_accessor :data, :next
+
+  def initialize(data)
+    @data = data
+  end
+end
+
+class SinglyLinkedList
+  attr_accessor :head
+
+  def initialize(head)
+    @head = head
+  end
+
+  def insert_node(data)
+    node = Node.new(data)
+    @head.next = @head
+    @head = node
   end
 end
 
@@ -13,12 +31,17 @@ node_2 = Node.new('node 2')
 node_3 = Node.new('node 3')
 node_4 = Node.new('node 4')
 
-node_1.next = node_2
+linked_list = SinglyLinkedList.new(node_1)
+
+linked_list.head.next = node_2
 node_2.next = node_3
 node_3.next = node_4
 
-puts "Node 1 is: #{node_1.node}"
-puts "Next after node 1 is: #{node_1.next.node}"
-puts "Next after node 2 is: #{node_2.next.node}"
-puts "Next after node 3 is: #{node_3.next.node}"
+puts "Head node is: #{node_1.data}"
+puts "Next after node 1 is: #{node_1.next.data}"
+puts "Next after node 2 is: #{node_2.next.data}"
+puts "Next after node 3 is: #{node_3.next.data}"
 puts "Next after node 4 is: #{node_4.next.inspect}"
+
+linked_list.insert_node('new head node')
+puts "New head node: #{linked_list.head.data}"
