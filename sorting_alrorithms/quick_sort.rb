0bd09cc0dd6a86149150eb61d8607ebd9cd115dd
John Figueiredo <john@johnfigueiredo.com>
2016-08-27 10:37:13 -0700

    Implement my_quick_sort and quick_sort
    - This was taken from somewhere else and I need to fully understand how this
    works because it is an incredibly powerful algorithm
    - TODO fix this up later to have one method that you have implemented the
    quicksort algorithm correctly

--- sorting_alrorithms/quick_sort.rb
+++ sorting_alrorithms/quick_sort.rb
@@ -5,23 +5,86 @@ require 'pry'
 # Time complexity in worst case is O(n^2) only when pivot is highest or lowest in array
 # Time complexity normally is O(n log n)
 
-def quick_sort(array, sub_array = [])
-  pivot = array[-1]
-  array.each_with_index do |item, index|
-    if item <= pivot
-      array[index] = array[0]
-      array[0] = item
-      sub_array << array.shift
+# This does not call the method on both smaller and bigger arrays
+def my_quick_sort(array, pivot_index = 0)
+  iteration_count = array.count - pivot_index
+  initial_pivot_index = pivot_index
+
+  iteration_count.times do |index|
+    item = array[index+initial_pivot_index]
+    if item <= array[-1]
+      array[index+initial_pivot_index] = array[pivot_index]
+      array[pivot_index] = item
+      pivot_index += 1
     end
   end
 
-  p "Sub array: #{sub_array}, Array: #{array}"
-  return if array.empty?
-  quick_sort(array, sub_array)
+  p "Array: #{array}"
+  return if array.count == pivot_index
+  quick_sort(array, pivot_index)
 end
 
-p '=== Quick sort 1 ==='
-quick_sort([6,5,1,3,8,4,7,9,2]) #= [1,2,3,4,5,6,7,8,9]
-p '=== Quick sort 2 ==='
-quick_sort([2,3,1,4,5,6,7,8,9]) #= [1,2,3,4,5,6,7,8,9]
+def quick_sort(array, from=0, to=nil)
+  if to == nil
+    # Sort the whole array, by default
+    to = array.count - 1
+  end
 
+  if from >= to
+    # Done sorting
+    return
+  end
+
+  # Take a pivot value, at the far left
+  pivot = array[from]
+
+  # Min and Max pointers
+  min = from
+  max = to
+
+  # Current free slot
+  free = min
+
+  while min < max
+    if free == min # Evaluate array[max]
+      if array[max] <= pivot # Smaller than pivot, must move
+        array[free] = array[max]
+        min += 1
+        free = max
+      else
+        max -= 1
+      end
+    elsif free == max # Evaluate array[min]
+      if array[min] >= pivot # Bigger than pivot, must move
+        array[free] = array[min]
+        max -= 1
+        free = min
+      else
+        min += 1
+      end
+    else
+      raise "Inconsistent state"
+    end
+  end
+
+  array[free] = pivot
+
+  p array
+  quick_sort array, from, free - 1
+  quick_sort array, free + 1, to
+end
+
+p '=== My quicksort 1 ==='
+p "Beginning Array: #{[6,5,1,3,8,4,7,9,2]}"
+my_quick_sort([6,5,1,3,8,4,7,9,2]) #= [1,2,3,4,5,6,7,8,9]
+
+# TODO fix when left of wall array is unsorted
+p '=== My quicksort 2 ==='
+p "Beginning Array: #{[2,3,1,4,5,6,7,8,9]}"
+my_quick_sort([2,3,1,4,5,6,7,8,9]) #= [1,2,3,4,5,6,7,8,9]
+
+p '=== right quicksort 1 ==='
+quick_sort([6,5,1,3,8,4,7,9,2]) #= [1,2,3,4,5,6,7,8,9]
+
+p '=== right quicksort 2 ==='
+quick_sort([2,3,1,4,5,6,7,8,9]) #= [1,2,3,4,5,6,7,8,9]
