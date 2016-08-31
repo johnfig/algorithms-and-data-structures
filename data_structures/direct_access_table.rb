670e988114d97843a50a407895a799be41b00c64
John Figueiredo <pair+john.figueiredo@goodsearch.com>
2016-08-30 17:34:36 -0700

    Implement direct_access_table to basically show what an array is

---
+++ data_structures/direct_access_table.rb
@@ -0,0 +1,10 @@
+# Assumptions
+# 1. Keys are drawn from a set of integers, s= {0,1,2,3...,m-1}
+# 2. Keys are unique
+# Basically these are arrays
+
+direct_access_table = ['cat','dog','bird','yetti']
+p direct_access_table[0]
+p direct_access_table[1]
+p direct_access_table[2]
+p direct_access_table[3]
